ceb81a337331f0f3557f1ca295e8b5244ff5f952
Patricio López <lopezjuripatricio@gmail.com>
2016-10-31 11:33:29 -0300

    Setup validation route

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.3.1'
 
 gem 'sinatra', '~> 1.4'
 gem 'puma', '~> 3.6'
+gem 'json', '~> 1.8'
--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require 'sinatra/base'
+require 'json'
+
+# HTTP responses
+class Responses
+  def self.ok
+    [200, { message: 'OK' }.to_json]
+  end
+
+  def self.unauthorized
+    [401, { message: 'No Autorizado' }.to_json]
+  end
+end
 
 # WebService2 as a modular Sinatra app for testing proposes
 class App < Sinatra::Base
@@ -11,6 +23,28 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    'Hello world!'
+    "This is a private service, so you shouldn't be able to see this."
+  end
+
+  get '/rest/verify_user/:email?' do |email|
+    content_type :json
+    Responses.unauthorized if email.nil?
+
+    begin
+      _ = JSON.parse(request.body.read)
+      # TODO: custom logic
+    rescue
+      return Responses.unauthorized # TODO: create bad-request response
+    end
+
+    # Current temporal validation
+    case email
+    when 'ignacio@acid.cl'
+      Responses.unauthorized
+    when 'iverdejo@acid.cl'
+      Responses.ok
+    else
+      Responses.unauthorized
+    end
   end
 end
